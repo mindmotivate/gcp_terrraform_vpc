9cabbc83d1a4f089213d1d79fb7638efea73e40b
mindmotivate <mindmotivate@gmail.com>
2024-04-26 22:50:43 -0400

    third commit

--- main.tf
+++ main.tf
@@ -1,38 +1,51 @@
-resource "google_compute_network" "auto-vpc-tf" {
-  name = "auto-vpc-tf"
-  auto_create_subnetworks = true
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.25.0"
+    }
+  }
 }
 
-resource "google_compute_network" "custom-vpc-tf" {
-  name = "custom-vpc-tf"
+provider "google" {
+  # Configuration options
+  region = "us-central1"
+  project = "thisismustafar"
+  zone = "us-central1-a"
+  credentials = "thisismustafar-98bc057b15db.json"
+
+}
+
+resource "google_storage_bucket" "bucket1" {
+  name          = "copy_this_and_chewbacca_will_raw_wookie_you_no_diddy"
+  location      = "us-central1"
+  force_destroy = true
+
+}
+
+
+resource "google_compute_network" "auto-vpc-tf" {
+  name = "auto-vpc-tf"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "sub-sg" {
-  name = "sub-sg"
-  network = google_compute_network.custom-vpc-tf.id
-  ip_cidr_range = "10.1.0.0/24"
-  region = "asia-southeast1"
-  private_ip_google_access = true
-
-}
-
-resource "google_compute_firewall" "allow-icmp" {
-  name = "allow-icmp"
-  network = google_compute_network.custom-vpc-tf.id
-  allow {
-    protocol = "icmp"
-  }
-  source_ranges = ["49.36.82.10/32"]
-  priority = 455
+  name ="sub-sg"
+  network = google_compute_network.auto-vpc-tf.id
+  ip_cidr_range = "10.72.1.0/24"
+  region = "asia-northeast1"
 }
 
 
+#resource "google_compute_network" "custom-vpc-tf" {
+  #name = "custom-vpc-tf"
+ #auto_create_subnetworks = false
+#}
 
 output "auto" {
   value = google_compute_network.auto-vpc-tf.id
 }
 
-output "custom" {
-  value = google_compute_network.custom-vpc-tf.id
-}
+#output "custom" {
+#  value = google_compute_network.custom-vpc-tf.id
+#}
