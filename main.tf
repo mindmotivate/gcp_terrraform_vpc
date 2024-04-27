b5612a88af813eda28ec32d01bd6e29d62087e05
mindmotivate <mindmotivate@gmail.com>
2024-04-27 10:56:36 -0400

    fourth commit

--- main.tf
+++ main.tf
@@ -1,41 +1,36 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "5.25.0"
     }
   }
 }
 
 provider "google" {
-  # Configuration options
-  region = "us-central1"
-  project = "thisismustafar"
-  zone = "us-central1-a"
-  credentials = "thisismustafar-98bc057b15db.json"
-
+  project     = "theta-style-416117"   # Updated project name
+  region      = "asia-northeast1"        # Updated region
+  zone        = "us-central1-a"          # Updated zone
+  credentials = "theta-style-416117-95ff7de18b6e.json"              # Updated credentials file
 }
 
 resource "google_storage_bucket" "bucket1" {
-  name          = "copy_this_and_chewbacca_will_raw_wookie_you_no_diddy"
+  name          = "malgus_clan_bucket"
   location      = "us-central1"
   force_destroy = true
-
 }
 
-
 resource "google_compute_network" "auto-vpc-tf" {
-  name = "auto-vpc-tf"
+  name                  = "auto-vpc-tf"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "sub-sg" {
-  name ="sub-sg"
-  network = google_compute_network.auto-vpc-tf.id
-  ip_cidr_range = "10.72.1.0/24"
-  region = "asia-northeast1"
+  name               = "sub-sg"
+  network            = google_compute_network.auto-vpc-tf.id
+  ip_cidr_range      = "10.72.1.0/24"
+  region             = "asia-northeast1"
 }
-
 
 #resource "google_compute_network" "custom-vpc-tf" {
   #name = "custom-vpc-tf"
