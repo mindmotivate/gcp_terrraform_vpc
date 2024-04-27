08e9ec56e5989e89f98a35225eaac5445136cb0f
mindmotivate <mindmotivate@gmail.com>
2024-04-27 15:11:30 -0400

    fifth commit

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "google" {
   project     = "theta-style-416117"   # Updated project name
   region      = "asia-northeast1"        # Updated region
-  zone        = "us-central1-a"          # Updated zone
+  zone        = "us-central1"          # Updated zone
   credentials = "theta-style-416117-95ff7de18b6e.json"              # Updated credentials file
 }
 
@@ -29,13 +29,33 @@ resource "google_compute_subnetwork" "sub-sg" {
   name               = "sub-sg"
   network            = google_compute_network.auto-vpc-tf.id
   ip_cidr_range      = "10.72.1.0/24"
-  region             = "asia-northeast1"
+  region             = "us-central1"
 }
 
 #resource "google_compute_network" "custom-vpc-tf" {
   #name = "custom-vpc-tf"
  #auto_create_subnetworks = false
 #}
+
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "malgus-instance"
+  machine_type = "n2-standard-4"
+  zone         = "us-central1-a"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+  network_interface {
+    subnetwork = google_compute_subnetwork.sub-sg.id
+    access_config {}
+  }
+  metadata_startup_script = "echo 'Hello, world! This is a startup script.' > /var/www/html/index.html"
+}
+
+
+
 
 output "auto" {
   value = google_compute_network.auto-vpc-tf.id
@@ -44,3 +64,8 @@ output "auto" {
 #output "custom" {
 #  value = google_compute_network.custom-vpc-tf.id
 #}
+
+
+output "vm_access_link" {
+  value = "http://${google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip}"
+}
