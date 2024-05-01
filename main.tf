86dfe7b3eea7b947f9c3a34c3b774edc6d3d17e6
mindmotivate <mindmotivate@gmail.com>
2024-04-30 20:22:39 -0400

    seventh commit

--- main.tf
+++ main.tf
@@ -10,58 +10,37 @@ terraform {
 provider "google" {
   project     = "theta-style-416117"   # Updated project name
   region      = "asia-northeast1"        # Updated region
-  zone        = "us-central1"          # Updated zone
-  credentials = "theta-style-416117-95ff7de18b6e.json"              # Updated credentials file
+  zone        = "us-east1"          # Updated zone
+  credentials = "theta-style-416117-95ff7de18b6e.json"  # Updated credentials file
 }
 
-resource "google_storage_bucket" "bucket1" {
-  name          = "malgus_clan_bucket"
-  location      = "us-central1"
+resource "google_storage_bucket" "malgusclan_bucket" {
+  name          = "malgusclan_bucket"
+  location      = "us-east1"
   force_destroy = true
 }
 
-resource "google_compute_network" "auto-vpc-tf" {
-  name                  = "auto-vpc-tf"
+resource "google_compute_network" "malgusclan_vpc" {
+  name                  = "malgusclan_vpc"
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "sub-sg" {
-  name               = "sub-sg"
-  network            = google_compute_network.auto-vpc-tf.id
-  ip_cidr_range      = "10.72.1.0/24"
+resource "google_compute_subnetwork" "malgusclan_subnetwork" {
+  name               = "malgusclan_subnetwork"
+  network            = google_compute_network.malgusclan_vpc.id
+  ip_cidr_range      = "10.136.1.0/24"
   region             = "us-central1"
 }
 
 #resource "google_compute_network" "custom-vpc-tf" {
-  #name = "custom-vpc-tf"
- #auto_create_subnetworks = false
+#  name = "custom-vpc-tf"
+#  auto_create_subnetworks = false
 #}
 
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "malgus-instance"
-  machine_type = "n2-standard-4"
-  zone         = "us-central1-a"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-10"
-    }
-  }
-  network_interface {
-    subnetwork = google_compute_subnetwork.sub-sg.id
-    access_config {}
-  }
-  metadata_startup_script = "echo 'Hello, world! This is a startup script.' > /var/www/html/index.html"
-}
-
-
-
-
 output "auto" {
-  value = google_compute_network.auto-vpc-tf.id
+  value = google_compute_network.malgusclan_vpc.id
 }
 
 #output "custom" {
 #  value = google_compute_network.custom-vpc-tf.id
 #}
-
